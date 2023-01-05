bca8f906f3141d469c02cf5ff291e8c3db8acb24
Fatima Ferreira <ifatinha99@gmail.com>
2023-01-05 13:01:17 -0300

    about.html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+/** AOS Animation */
 AOS.init();
+
+/** ativar link na página */
+const links = document.querySelectorAll("#menu-desktop .nav-desktop a");
+
+function ativarLink(link) {
+    const url = location.href;
+    const lHref = link.href;
+
+    if (url.includes(lHref)) {
+        link.classList.add("ativo");
+    }
+}
+
+links.forEach(ativarLink);
